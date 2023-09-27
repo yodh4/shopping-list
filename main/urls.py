e61ca2495eda137323c4b64abcb807c4964fa463
yodh4 <cyrilus2004@gmail.com>
2023-09-27 15:35:43 +0700

    Selesai tutorial 4

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,8 @@ from main.views import show_main, create_product, show_xml, show_json, show_xml_
 from main.views import register
 from main.views import login_user
 from main.views import logout_user
+from main.views import edit_product
+from main.views import delete_product
 
 app_name = 'main'
 
@@ -16,4 +18,6 @@ urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
+    path('edit-product/<int:id>', edit_product, name='edit_product'),
+    path('delete/<int:id>', delete_product, name='delete_product'),
 ]
